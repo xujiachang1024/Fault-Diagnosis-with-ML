753a42bb2bf969ad35a0d9da2b409afce1fe6742
Ernest Xu 徐嘉昌 <ernest.xu1024@gmail.com>
2019-06-24 10:00:02 +0100

    MEISVM paper: implement feature 3, the impulse factor

---
+++ references/MEISVM/f3_impulse_factor.m
@@ -0,0 +1,9 @@
+%{
+    Feature 3: the impulse factor
+    @param x: a signal seris
+    @param N: the number of data points
+    @return F3: feature 3, the impulse
+%}
+function F3 = f3_impulse_factor(x, N)
+F3 = maxabs(x) / (sumabs(x) / N);
+end
