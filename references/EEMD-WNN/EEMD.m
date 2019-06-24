904e14ecb1fcbad56f8b70be7e2db87aeb42933e
Ernest Xu 徐嘉昌 <ernest.xu1024@gmail.com>
2019-06-24 09:32:02 +0100

    Start to implement EEMD method

---
+++ references/EEMD-WNN/EEMD.m
@@ -0,0 +1,10 @@
+%{
+    EEMD algorithm
+    @param x: the original, noise-added signal
+    @param M: the number of ensemble
+    @param I: the number of intrinsic mode functions (IMFs)
+    @return A: the means of each of the IMFs
+%}
+function A = EEMD(x, M, I)
+
+end
