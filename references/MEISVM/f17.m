7f270f98215609a5f2b99f999cd407560675ca4b
Ernest Xu 徐嘉昌 <ernest.xu1024@gmail.com>
2019-06-24 16:32:46 +0100

    MEISVM paper: implement feature 17

---
+++ references/MEISVM/f17.m
@@ -0,0 +1,16 @@
+%{
+    Feature 17
+    @param s: the spectrums
+    @param K: the number of spectrum lines
+    @param f: the frequency values of the spectrum lines
+    @param F11: feature 11
+    @param F12: feature 12
+    @return F17: feature 17
+%}
+function F17 = f17(s, K, f, F11, F12)
+    sum_numerator = 0.0;
+    for k=1:K
+        sum_numerator = sum_numerator + (f(k) - F11) .^ 3 * s(k);
+    end
+    F17 = sum_numerator / (K * F12 .^ 3));
+end
