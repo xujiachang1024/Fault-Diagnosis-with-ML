ca0a082437992e7d572325f2e8d7f4f0edcfba3f
Ernest Xu 徐嘉昌 <ernest.xu1024@gmail.com>
2019-06-24 09:42:40 +0100

    MEISVM paper: implement feature 1, the shape factor

---
+++ references/MEISVM/f1_shape_factor.m
@@ -0,0 +1,9 @@
+%{
+    Feature 1: the shape factor
+    @param x: a signal seris
+    @param N: the number of data points
+    @return F1: feature 1, the shape factor
+%}
+function F1 = f1_shape_factor(x, N)
+F1 = sqrt(sumsqr(x) / N) / (sumabs(x) / N);
+end
