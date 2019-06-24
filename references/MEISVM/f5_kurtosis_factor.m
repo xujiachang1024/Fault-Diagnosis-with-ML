d1b0beb8a31bd927626cc35ff7d6fd4e0ff7ef3f
Ernest Xu 徐嘉昌 <ernest.xu1024@gmail.com>
2019-06-24 10:42:12 +0100

    MEISVM paper: implement feature 5, the kurtosis factor

---
+++ references/MEISVM/f5_kurtosis_factor.m
@@ -0,0 +1,18 @@
+%{
+    Feature 5: the kurtosis factor
+    @param x: a signal seris
+    @param N: the number of data points
+    @return F1: feature 5, the kurtosis factor
+%}
+function F5 = f5_kurtosis_factor(x, N)
+x_mean = mean(x);
+sum_numerator = 0.0;
+for n=1:N
+    sum_numerator = sum_numerator + (x(n) - x_mean) .^ 4;
+end
+sum_denominator = 0.0;
+for n=1:N
+    sum_denominator = sum_denominator + (x(n) - x_mean) .^ 2;
+end
+F5 = (sum_numerator / N) / (sqrt(sum_denominator / N) .^ 4);
+end
