47456342cd7e93a96eff3bf656c602fd51f71d26
Ernest Xu 徐嘉昌 <ernest.xu1024@gmail.com>
2019-06-24 15:05:53 +0100

    MEISVM paper: implement feature 9

---
+++ references/MEISVM/f9.m
@@ -0,0 +1,15 @@
+%{
+    Feature 9
+    @param s: a spectrum
+    @param K: the number of spectrum lines
+    @param F7: feature 7, the vibration energy in the frequency domain
+    @param F8: feature 8
+    @return F9: feature 9
+%}
+function F9 = f8(s, K, F7, F8)
+    sum_numerator = 0.0
+    for k=1:K
+        sum_numerator = sum_numerator + (s(k) - F7) .^ 3;
+    end
+    F9 = sum_numerator / (K * (sqrt(F8) .^ 3));
+end
