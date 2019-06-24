07a213b61d9b2c98a64cc8ee48f3c170398b7666
Ernest Xu 徐嘉昌 <ernest.xu1024@gmail.com>
2019-06-24 15:39:56 +0100

    MEISVM paper: implement feature 11

---
+++ references/MEISVM/f11.m
@@ -0,0 +1,14 @@
+%{
+    Feature 11
+    @param s: the spectrums
+    @param K: the number of spectrum lines
+    @param f: the frequency values of the spectrum lines
+    @return F11: feature 11
+%}
+function F11 = f11(s, K, F7, F8)
+    sum_numerator = 0.0
+    for k=1:K
+        sum_numerator = sum_numerator + f(k) * s(k);
+    end
+    F11 = sum_numerator / sum(s);
+end
