e5f152e8de108423f586d651d6b66d3fb84f7a93
Ernest Xu 徐嘉昌 <ernest.xu1024@gmail.com>
2019-06-25 10:30:45 +0100

    EEMD-WNN paper: implement EEMD method

--- references/EEMD-WNN/EEMD.m
+++ references/EEMD-WNN/EEMD.m
@@ -1,10 +1,22 @@
 %{
     EEMD algorithm
-    @param x: the original, noise-added signal
+    @param x: the column vector of the original, noise-added signal
     @param M: the number of ensemble
     @param I: the number of intrinsic mode functions (IMFs)
-    @return A: the means of each of the IMFs
+    @return a: the means of each of the IMFs
 %}
-function A = EEMD(x, M, I)
-
+function a = EEMD(x, M, I)
+    T, _ = size(x)
+    imf = zeros(T, I, M)
+    for m=1:M
+        % add a different white noise series $n_m with the given amplitude to the investigate signal $x
+        x_m = x + n_m;
+        % decompose the noise-added signal $x_m into I IMFs
+        imf(:, :, M), residual = emd(x_m, 'MaxNumIMF', I);
+    end
+    % calculate the ensemble mean of the M trials for each IMF
+    a = zeros(T, I)
+    for i=1:I:
+        a(:, i) = mean(imf(i, :, :), 3);
+    end
 end
