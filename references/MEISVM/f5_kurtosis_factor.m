ff2d5a5f25942bb137834cdbad3837f75a91a561
Ernest Xu 徐嘉昌 <ernest.xu1024@gmail.com>
2019-06-26 08:19:58 +0100

    MEISVM paper: update feature 5, the kurtosis factor

--- references/MEISVM/f5_kurtosis_factor.m
+++ references/MEISVM/f5_kurtosis_factor.m
@@ -5,14 +5,14 @@
     @return F5: feature 5, the kurtosis factor
 %}
 function F5 = f5_kurtosis_factor(x, N)
-x_mean = mean(x);
-sum_numerator = 0.0;
-for n=1:N
-    sum_numerator = sum_numerator + (x(n) - x_mean) .^ 4;
-end
-sum_denominator = 0.0;
-for n=1:N
-    sum_denominator = sum_denominator + (x(n) - x_mean) .^ 2;
-end
-F5 = (sum_numerator / N) / (sqrt(sum_denominator / N) .^ 4);
+    x_mean = mean(x);
+    sum_numerator = 0.0;
+    for n=1:N
+        sum_numerator = sum_numerator + (x(n) - x_mean) .^ 4;
+    end
+    sum_denominator = 0.0;
+    for n=1:N
+        sum_denominator = sum_denominator + (x(n) - x_mean) .^ 2;
+    end
+    F5 = (sum_numerator / N) / (sqrt(sum_denominator / N) .^ 4);
 end
