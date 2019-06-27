f1c0e06342ad1330c32535c05616b7eff234cd23
Ernest Xu 徐嘉昌 <ernest.xu1024@gmail.com>
2019-06-27 12:17:32 +0100

    MEISVM paper: update combined features method

--- references/MEISVM/features_19.m
+++ references/MEISVM/features_19.m
@@ -3,28 +3,13 @@
     @param x: a signal seris
     @param N: the number of data points
     @param sf: the sampling frequency
-    @param plot: whether to plot the spectrum
     @return F: the feature vector
 %}
-function F = features_19(x, N, sf, plot)
-    % compute the Fourier transform of the signal
-    Y = fft(x);
-    % compute the two-sided spectrum P2
-    P2 = abs(Y / N);
-    % compute the single-sided spectrum P1
-    P1 = P2(1:(N / 2 + 1));
-    P1(2:end-1) = 2 * P1(2:end-1);
-    P1
-    % define the fequency domain fd and plot the single-sided spectrum P1
-    if plot ~= false
-        fd = sf * (0:(N/2)) / N;
-        plot(fd, P1)
-        title('Single-Sided Amplitude Spectrum of x(t)')
-        xlabel('frequency domain (Hz)')
-        ylabel('|P1(f)|')
-    end
+function F = features_19(x, N, sf)
+    % compute the spectrum P1
+    [fd, P1] = compute_spectrum(x, N, sf);
     % compute the feature vector F
-%     F = zeros(19, 1);
+    F = zeros(19, 1);
 %     F(1, :) = f1_shape_factor(x, N);
 %     F(2, :) = f2_crest_factor(x, N);
 %     F(3, :) = f3_impulse_factor(x, N);
