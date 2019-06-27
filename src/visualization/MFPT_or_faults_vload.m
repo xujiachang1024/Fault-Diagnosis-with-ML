9b47a7517fcc0cde13ad72f6facccbf8eeb57360
Ernest Xu 徐嘉昌 <ernest.xu1024@gmail.com>
2019-06-27 11:04:42 +0100

    Visualise MFPT outer race faults (variable loads) vibration signal

---
+++ src/visualization/MFPT_or_faults_vload.m
@@ -0,0 +1,17 @@
+clear;
+load('data/interim/MFPT/or_faults_vload.mat');
+loads = [25 50 100 150 200 250 300];
+% get the length of signal N, and the number of samples M
+[N, M] = size(x);
+t = [1:N];
+figure();hold on
+for m=1:M
+    subplot(M, 1, m);
+    plot(t, x(:, m)');
+    pbaspect([80 1 1]);
+%     set(gca,'LooseInset',get(gca,'TightInset'));
+    title(strcat('Vibration Signal of MFPT Outter Race Fault #', num2str(m), ', load=', num2str(loads(m))));
+    xlabel('t');
+    ylabel('x(t)');
+    ylim([-5 5]);
+end
