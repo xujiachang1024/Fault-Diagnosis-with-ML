dd9f477ee09021d7bc3a897d561674f1b993189e
Ernest Xu 徐嘉昌 <ernest.xu1024@gmail.com>
2019-06-27 10:49:32 +0100

    Splice MFPT outer race fault data subset

--- src/cleaning/MFPT/or_fault_cleaning.m
+++ src/cleaning/MFPT/or_fault_cleaning.m
@@ -1,9 +1,20 @@
 clear;
+% read
 data_source_directory = 'data/external/MFPT/2 - Three Outer Race Fault Conditions/';
 for i=1:3
     data_file_path = strcat(data_source_directory, 'OuterRaceFault_', num2str(i), '.mat');
     load(data_file_path);
     x(:, i) = bearing.gs;
 end
+% splice
+cache = x;
+clear x;
+N = 146484;
+M = 12;
+x(:, 1:3) = cache(1:146484, :);
+x(:, 4:6) = cache(146485:292968, :);
+x(:, 7:9) = cache(292969:439452, :);
+x(:, 10:12) = cache(439453:585936, :);
+% write
 data_sink_path = 'data/interim/MFPT/or_faults.mat';
 save(data_sink_path, 'x');
