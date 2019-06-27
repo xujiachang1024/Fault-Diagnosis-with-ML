b6aa14df46a058bab877af4fda7e497659fe42da
Ernest Xu 徐嘉昌 <ernest.xu1024@gmail.com>
2019-06-27 12:19:32 +0100

    MEISVM paper: implement compute_spectrum() method using FFT

---
+++ references/MEISVM/compute_spectrum.m
@@ -0,0 +1,18 @@
+%{
+    Compute the spectrum
+    @param x: a signal seris
+    @param N: the number of data points
+    @param sf: the sampling frequency
+    @return fd: the frequency domain
+    @return P1: the single-sided spectrum
+%}
+function [fd, P1] = compute_spectrum(x, N, sf)
+    % compute the frequency domain
+    fd = (sf * (0:(N/2)) / N)';
+    % compute the Fourier transform of the signal
+    Y = fft(x);
+    % compute the two-sided spectrum P2
+    P2 = abs(Y / N);
+    % compute the single-sided spectrum P1
+    P1 = P2(1:(N / 2 + 1));
+    P1(2:end-1) = 2 * P1(2:end-1);
