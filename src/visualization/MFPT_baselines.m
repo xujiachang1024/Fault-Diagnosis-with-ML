6cd21c6259b385471abb1f424029fec370b2d591
Ernest Xu 徐嘉昌 <ernest.xu1024@gmail.com>
2019-06-27 10:54:37 +0100

    Visualise MFPT baseline vibration signal

---
+++ src/visualization/MFPT_baselines.m
@@ -0,0 +1,15 @@
+clear;
+load('data/interim/MFPT/baselines.mat');
+% get the length of signal N, and the number of samples M
+[N, M] = size(x);
+t = [1:N];
+figure();hold on
+for m=1:M
+    subplot(M, 1, m);
+    plot(t, x(:, m)');
+    pbaspect([80 1 1]);
+%     set(gca,'LooseInset',get(gca,'TightInset'));
+    title(strcat('Vibration Signal of MFPT Baseline #', num2str(m)));
+    xlabel('t');
+    ylabel('x(t)');
+end
