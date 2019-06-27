d1ef2d2c3a25cf5321519b08f9b26421c5fcffbb
Ernest Xu 徐嘉昌 <ernest.xu1024@gmail.com>
2019-06-27 12:18:38 +0100

    MEISVM paper: test unit for compute_spectrum() method

--- references/MEISVM/features_19_test.m
+++
@@ -1,7 +0,0 @@
-clear;
-load('/home/k1898015/Fault-Diagnosis-with-ML/data/external/MFPT/1 - Three Baseline Conditions/baseline_1.mat');
-x = bearing.gs;
-[N, ncol] = size(x);
-fs = 97656;
-plot = true;
-F = features_19(x, N, sf, plot);
