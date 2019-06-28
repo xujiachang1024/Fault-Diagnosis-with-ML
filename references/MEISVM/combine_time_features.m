2fcd5a8671addc0129c04e2b6b18813c673d5923
Ernest Xu 徐嘉昌 <ernest.xu1024@gmail.com>
2019-06-28 09:30:40 +0100

    MEISVM paper: update the method to combine time-domain features

--- references/MEISVM/combine_time_features.m
+++ references/MEISVM/combine_time_features.m
@@ -1,24 +1,21 @@
 %{
     Combine the time-domain features
-    @param x: a signal matrix
+    @param x: a signal series
+    @param N: the number of data points
     @return F_time: the time-domain feature vector
 %}
-function F_time = combine_time_features(x)
-    [N, M] = size(x);
-    F_time = zeros(6, M);
-    for m=1:M
-        % combine the shape factor
-        F_time(1, m) = f1_shape_factor(x(:, m), N);
-        % combine the crest factor
-        F_time(2, m) = f2_crest_factor(x(:, m), N);
-        % combine the impulse factor
-        F_time(3, m) = f3_impulse_factor(x(:, m), N);
-        % combine the margin factor
-        F_time(4, m) = f4_margin_factor(x(:, m), N);
-        % combine the kurtosis factor
-        F_time(5, m) = f5_kurtosis_factor(x(:, m), N);
-        % combine the skewness factor
-        F_time(6, m) = f6_skewness_factor(x(:, m), N);
-    end
+function F_time = combine_time_features(x, N)
+    % combine the shape factor
+    F_time(1, :) = f1_shape_factor(x, N);
+    % combine the crest factor
+    F_time(2, :) = f2_crest_factor(x, N);
+    % combine the impulse factor
+    F_time(3, :) = f3_impulse_factor(x, N);
+    % combine the margin factor
+    F_time(4, :) = f4_margin_factor(x, N);
+    % combine the kurtosis factor
+    F_time(5, :) = f5_kurtosis_factor(x, N);
+    % combine the skewness factor
+    F_time(6, :) = f6_skewness_factor(x, N);
 end
 
