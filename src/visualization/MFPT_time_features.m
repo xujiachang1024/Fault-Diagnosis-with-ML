cfeca719a0f621a0febea7f0d929bae03fee9372
Ernest Xu 徐嘉昌 <ernest.xu1024@gmail.com>
2019-06-28 11:37:58 +0100

    MEISVM paper: visualise time-domain features for the MFPT data set

--- src/visualization/MFPT_time_features.m
+++ src/visualization/MFPT_time_features.m
@@ -1,6 +1,7 @@
 clear;
+ss = [144 144 84 84];
 F = 6;
-F_time = zeros(F, 12 + 12 + 7 + 7);
+F_time = zeros(F, sum(ss));
 
 % combine time-domain features for baselines
 load('data/interim/MFPT/baselines.mat');
@@ -13,30 +14,30 @@ end
 load('data/interim/MFPT/or_faults.mat');
 [N, M] = size(x);
 for m=1:M
-    F_time(:, 12 + m) = combine_time_features(x(:, m), N);
+    F_time(:, ss(1) + m) = combine_time_features(x(:, m), N);
 end
 
 % combine time-domain features for OR faults (variable loads)
 load('data/interim/MFPT/or_faults_vload.mat');
 [N, M] = size(x);
 for m=1:M
-    F_time(:, 12 + 12 + m) = combine_time_features(x(:, m), N);
+    F_time(:, sum(ss(1:2)) + m) = combine_time_features(x(:, m), N);
 end
 
 % combine time-domain features for IR faults (variable loads)
 load('data/interim/MFPT/ir_faults_vload.mat');
 [N, M] = size(x);
 for m=1:M
-    F_time(:, 12 + 12 + 7 + m) = combine_time_features(x(:, m), N);
+    F_time(:, sum(ss(1:3)) + m) = combine_time_features(x(:, m), N);
 end
 
 figure();hold on
 for f=1:F
     subplot(3, 2, f);hold on
-    plot(F_time(f, 1:12));
-    plot(F_time(f, 13:24));
-    plot(F_time(f, 25:31));
-    plot(F_time(f, 32:38));
+    plot(F_time(f, 1:ss(1)));
+    plot(F_time(f, (ss(1)+1):sum(ss(1:2))));
+    plot(F_time(f, (sum(ss(1:2))+1):sum(ss(1:3))));
+    plot(F_time(f, (sum(ss(1:3))+1):sum(ss)));
     title(strcat('Feature #', num2str(f)));
     xlabel('Sample point')
     ylabel(strcat('F', num2str(f)))
