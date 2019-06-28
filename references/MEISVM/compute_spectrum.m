c8aea12420f2e05e518c8af5fd8c1c3baac9a4a0
Ernest Xu 徐嘉昌 <ernest.xu1024@gmail.com>
2019-06-28 11:37:30 +0100

    MEISVM paper: update the method to compute spectrum

--- references/MEISVM/compute_spectrum.m
+++ references/MEISVM/compute_spectrum.m
@@ -1,6 +1,6 @@
 %{
     Compute the spectrum
-    @param x: a signal seris
+    @param x: a signal series
     @param N: the number of data points
     @param sf: the sampling frequency
     @return fd: the frequency domain
@@ -14,5 +14,5 @@ function [fd, P1] = compute_spectrum(x, N, sf)
     % compute the two-sided spectrum P2
     P2 = abs(Y / N);
     % compute the single-sided spectrum P1
-    P1 = P2(1:(N / 2 + 1));
+    P1 = P2(1:(int8(N / 2) + 1));
     P1(2:end-1) = 2 * P1(2:end-1);
