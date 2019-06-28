deb4c7dfcf73a58a8ccc24f5cff1530af4bb7330
Ernest Xu 徐嘉昌 <ernest.xu1024@gmail.com>
2019-06-28 08:45:12 +0100

    MEISVM paper: visualise time-domain features for the MFPT data set

---
+++ src/visualization/MFPT_time_features.m
@@ -0,0 +1,43 @@
+clear;
+F = 6;
+F_time = zeros(F, 12 + 12 + 7 + 7);
+
+% combine time-domain features for baselines
+load('data/interim/MFPT/baselines.mat');
+[N, M] = size(x);
+for m=1:M
+    F_time(:, m) = combine_time_features(x(:, m), N);
+end
+
+% combine time-domain features for OR faults
+load('data/interim/MFPT/or_faults.mat');
+[N, M] = size(x);
+for m=1:M
+    F_time(:, 12 + m) = combine_time_features(x(:, m), N);
+end
+
+% combine time-domain features for OR faults (variable loads)
+load('data/interim/MFPT/or_faults_vload.mat');
+[N, M] = size(x);
+for m=1:M
+    F_time(:, 12 + 12 + m) = combine_time_features(x(:, m), N);
+end
+
+% combine time-domain features for IR faults (variable loads)
+load('data/interim/MFPT/ir_faults_vload.mat');
+[N, M] = size(x);
+for m=1:M
+    F_time(:, 12 + 12 + 7 + m) = combine_time_features(x(:, m), N);
+end
+
+for f=1:F
+    figure();hold on
+    plot(F_time(f, 1:12));
+    plot(F_time(f, 13:24));
+    plot(F_time(f, 25:31));
+    plot(F_time(f, 32:38));
+    title(strcat('Feature #', num2str(f)));
+    legend('Baselines', 'OR Faults', 'OR Faults (vload)', 'IR Faults (vload)');
+    xlabel('Sample point')
+    ylabel(strcat('F', num2str(f)))
+end
