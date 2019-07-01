b73724f8b4fcf31c70ccbcc4b27a93f3f95f0a3f
Ernest Xu 徐嘉昌 <ernest.xu1024@gmail.com>
2019-07-01 10:57:40 +0100

    SSA-ANN paper: update the method of embedding

--- references/SSA-ANN/embedding.m
+++ references/SSA-ANN/embedding.m
@@ -4,7 +4,7 @@
     @param L: the window length
     @return X: the trajectory matrix
 %}
-function X = embed_trajectory_matrix(x, L)
+function X = embedding(x, L)
     K = N - L + 1;
     X = zeros(L, K);
     for k=1:K
