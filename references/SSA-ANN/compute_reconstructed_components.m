c64b4b897264012a684c8e3bbad3e89842de8076
Ernest Xu 徐嘉昌 <ernest.xu1024@gmail.com>
2019-07-01 19:32:53 +0100

    SSA-ANN paper: implement the method to compute reconstructed components

---
+++ references/SSA-ANN/compute_reconstructed_components.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = compute_reconstructed_components(inputArg1,inputArg2)
+%COMPUTE_RECONSTRUCTED_COMPONENTS Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
