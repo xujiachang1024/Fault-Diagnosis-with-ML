850b6f7295055e8855778ddb02c86601441c24e1
Ernest Xu 徐嘉昌 <ernest.xu1024@gmail.com>
2019-07-01 10:25:07 +0100

    SSA-ANN paper: update the method to embed trajectory matrix

--- references/SSA-ANN/embed_trajectory_matrix.m
+++ references/SSA-ANN/embed_trajectory_matrix.m
@@ -1,11 +1,11 @@
 %{
     Embed trajectory matrix
     @param x: a signal series (column vector)
-    @param K: the number of lagged vectors
     @param L: the window length
     @return X: the trajectory matrix
 %}
-function X = embed_trajectory_matrix(x, K, L)
+function X = embed_trajectory_matrix(x, L)
+    K = N - L + 1;
     X = zeros(L, K);
     for k=1:K
         X(:, k) = x(k:(K + L -1), :);
