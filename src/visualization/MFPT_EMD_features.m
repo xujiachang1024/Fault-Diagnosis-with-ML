099d4e8728c3a9508217fe4cde7fb7cef9c78d29
Ernest Xu 徐嘉昌 <ernest.xu1024@gmail.com>
2019-07-02 11:28:49 +0100

    Visualise the EMD features of the MFPT data set

--- src/visualization/MFPT_EMD_features.m
+++ src/visualization/MFPT_EMD_features.m
@@ -16,44 +16,32 @@ end
 load('data/interim/MFPT/or_faults.mat');
 [~, M] = size(x);
 for m=1:M
-    F_EMD(:, ss(1) + m) = combine_time_features(x(:, m), m_EMD);
+    F_EMD(:, ss(1) + m) = compute_EMD_feature(x(:, m), m_EMD);
 end
 
 % combine time-domain features for OR faults (variable loads)
 load('data/interim/MFPT/or_faults_vload.mat');
 [~, M] = size(x);
 for m=1:M
-    F_EMD(:, sum(ss(1:2)) + m) = combine_time_features(x(:, m), m_EMD);
+    F_EMD(:, sum(ss(1:2)) + m) = compute_EMD_feature(x(:, m), m_EMD);
 end
 
 % combine EMD features for IR faults (variable loads)
 load('data/interim/MFPT/ir_faults_vload.mat');
 [~, M] = size(x);
 for m=1:M
-    F_EMD(:, sum(ss(1:3)) + m) = combine_time_features(x(:, m), e_EMD);
+    F_EMD(:, sum(ss(1:3)) + m) = compute_EMD_feature(x(:, m), m_EMD);
 end
 
 figure();hold on
-for f=1:F
-    subplot(3, 2, f);hold on
-    plot(F_time(f, 1:ss(1)));
-    plot(F_time(f, (ss(1)+1):sum(ss(1:3))));
-    plot(F_time(f, (sum(ss(1:3))+1):sum(ss)));
+for f=1:m_EMD
+    subplot(2, 4, f);hold on
+    plot(F_EMD(f, 1:ss(1)));
+    plot(F_EMD(f, (ss(1)+1):sum(ss(1:2))));
+    plot(F_EMD(f, (sum(ss(1:2))+1):sum(ss(1:3))));
+    plot(F_EMD(f, (sum(ss(1:3))+1):sum(ss)));
     title(strcat('Feature #', num2str(f)));
     xlabel('Sample point')
     ylabel(strcat('F', num2str(f)))
-    legend('Baselines', 'OR Faults', 'IR Faults');
-end
-
-figure();hold on
-for f=1:F
-    subplot(3, 5, f);hold on
-    plot(F_freq(f, 1:ss(1)));
-    plot(F_freq(f, (ss(1)+1):sum(ss(1:2))));
-    plot(F_freq(f, (sum(ss(1:2))+1):sum(ss(1:3))));
-    plot(F_freq(f, (sum(ss(1:3))+1):sum(ss)));
-    title(strcat('Feature #', num2str(6 + f)));
-    xlabel('Sample point')
-    ylabel(strcat('F', num2str(6 + f)))
     legend('Baselines', 'OR Faults', 'OR Faults (vload)', 'IR Faults (vload)');
 end
