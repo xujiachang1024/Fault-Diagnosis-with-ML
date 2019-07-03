ca83f37dcad8a23f5052626a896d32c058369a33
Ernest Xu 徐嘉昌 <ernest.xu1024@gmail.com>
2019-07-03 08:09:59 +0100

    EEMD-WNN paper: update the method to compute EEMD features

--- references/EEMD-WNN/compute_EEMD_feature.m
+++ references/EEMD-WNN/compute_EEMD_feature.m
@@ -1,29 +1,29 @@
 %{
     EEMD algorithm
     @param x: a noise series
-    @param M: the number of ensemble
-    @param I: the number of intrinsic mode functions (IMFs)
+    @param M_ens: the number of ensemble
+    @param I_imf: the number of intrinsic mode functions (IMFs)
     @return a: the means of each of the IMFs
     @return f: a column vector of normalised energy of IMFs
 %}
-function [a, f] = compute_EEMD_feature(x, M, I)
-    [T, ~] = size(x);
-    imf = zeros(T, I, M);
-    for m=1:M
+function [a, f] = compute_EEMD_feature(x, M_ens, I_imf)
+    [N, ~] = size(x);
+    imf = zeros(N, I_imf, M_ens);
+    for m=1:M_ens
         % add a different white noise series $n_m with the given amplitude to the investigate signal $x
         n_m = randn(size(x)) / 100;
         x_m = x + n_m;
         % decompose the noise-added signal $x_m into I IMFs
-        [imf(:, :, M), ~] = emd(x_m, 'MaxNumIMF', I);
+        [imf(:, :, M_ens), ~] = emd(x_m, 'MaxNumIMF', I_imf);
     end
     % calculate the ensemble mean of the M trials for each IMF
-    a = zeros(T, I);
-    for i=1:I
+    a = zeros(N, I_imf);
+    for i=1:I_imf
         a(:, i) = mean(imf(:, i, :), 3);
     end
     % construct a column vector with the energy as element
-    e = zeros(I, 1);
-    for i=1:I
+    e = zeros(I_imf, 1);
+    for i=1:I_imf
         e(i, :) = sumsqr(a(:, i));
     end
     % normalise the feature vector, and make it a column vector
