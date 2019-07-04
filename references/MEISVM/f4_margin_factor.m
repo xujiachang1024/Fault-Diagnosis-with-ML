14b1e13757b1b3823ae577dd1891fa2d62833f83
Ernest Xu 徐嘉昌 <ernest.xu1024@gmail.com>
2019-07-04 19:27:18 +0100

    MEISVM paper: update feature #4, the margin factor

--- references/MEISVM/f4_margin_factor.m
+++ references/MEISVM/f4_margin_factor.m
@@ -5,9 +5,5 @@
     @return F4: feature 4, the margin factor
 %}
 function F4 = f4_margin_factor(x, N)
-    sum_sqrt_abs = 0.0;
-    for n=1:N
-        sum_sqrt_abs = sum_sqrt_abs + sqrt(abs(x(n)));
-    end
-    F4 = max(abs(x)) / (sum_sqrt_abs / N) .^ 2;
+    F4 = max(abs(x)) / (sum(sqrt(abs(x))) / N) .^ 2;
 end
