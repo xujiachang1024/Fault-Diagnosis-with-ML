e85dc16d7934249ce60aee79537129eb45ae9b1b
Ernest Xu 徐嘉昌 <ernest.xu1024@gmail.com>
2019-07-04 20:42:01 +0100

    MEISVM paper: update feature #14

--- references/MEISVM/f14.m
+++ references/MEISVM/f14.m
@@ -6,11 +6,5 @@
     @return F14: feature 14
 %}
 function F14 = f14(s, K, f)
-    sum_numerator = 0.0;
-    sum_denominator = 0.0;
-    for k=1:K
-        sum_numerator = sum_numerator + f(k) .^ 4 * s(k);
-        sum_denominator = sum_denominator + f(k) .^ 2 * s(k);
-    end
-    F14 = sqrt(sum_numerator / sum_denominator);
+    F14 = sqrt(sum((f .^ 4) .* s) / sum((f .^ 2) .* s));
 end
