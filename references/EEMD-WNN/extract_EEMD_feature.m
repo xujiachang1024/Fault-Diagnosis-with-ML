9b7f24d52a34e2689fccefe2c087d3b4c1410049
Ernest Xu 徐嘉昌 <ernest.xu1024@gmail.com>
2019-07-04 19:19:36 +0100

    EEMD-WNN paper: update the test unit to update the sensitive IMF

--- references/EEMD-WNN/extract_EEMD_feature.m
+++ references/EEMD-WNN/extract_EEMD_feature.m
@@ -1,25 +1,26 @@
 %{
     Select sensitive IMF after EEMD algoirithm
-    @param A: A_sensitive: the sensitive IMF (N, 1, M)
+    @param A:the sensitive IMF (N, 1, M)
     @return 
 %}
-function F = extract_EEMD_feature(A_sensitive)
+function F = extract_EEMD_feature(A)
     % N: the number of data points in each sample
     % M: the number of samples
-    [N, ~, M] = size(A_sensitive);
+    [N, ~, M] = size(A);
     F = zeros(10, M);
     for m=1:M
         % feature 1: standard deviation
-        F(1, m) = std(A_sensitive(:, 1, m));
+        F(1, m) = std(A(:, 1, m));
         % feature 2: kurtosis
-        F(2, m) = kurtosis(A_sensitive(:, 1, m));
+        F(2, m) = kurtosis(A(:, 1, m));
         % feature 3: shape factor
-        F(3, m) = sqrt(sum(A_sensitive(:, 1, m) .^ 2) / N) / (sumabs(A_sensitive(:, 1, m)) / N);
+        F(3, m) = sqrt(sum(A(:, 1, m) .^ 2) / N) / (sumabs(A(:, 1, m)) / N);
         % feature 4: impulse factor
+        F(4, m) = max(abs(A(:, 1, m))) / (sum(abs(A(:, 1, m))) / N);
         % feature 5: mean frequency
         % feature 6: root mean square frequency
         % feature 7: standard deviation frequency
-        % feature 8: pectrum peak ratio
+        % feature 8: spectrum peak ratio
     end 
 end
 
