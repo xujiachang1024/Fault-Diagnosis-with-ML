bbf0b4683dc4e7185855d3c49a2ddbe0fa00047a
Ernest Xu 徐嘉昌 <ernest.xu1024@gmail.com>
2019-07-04 11:46:14 +0100

    EEMD-WNN paper: update the test unit to select sensitive IMF

--- references/EEMD-WNN/select_sensitive_IMF_test.m
+++ references/EEMD-WNN/select_sensitive_IMF_test.m
@@ -7,6 +7,7 @@ load('data/interim/CWRU/a1_normal.mat');
 [N, M] = size(x);
 A_a1 = zeros(N, I_imf, M);
 for m=1:M
-    [A_a1(:, :, m), ~] = compute_EEMD_feature(x(:, m), M_ens, I_imf);
+    [A_a1(:, :, m), ~] = generate_ensemble_IMF(x(:, m), M_ens, I_imf);
 end
 [A_sensitive_a1, i_sensitive] = select_sensitive_IMF(A_a1, false);
+F = extract_EEMD_features(A_sensitive_a1);
