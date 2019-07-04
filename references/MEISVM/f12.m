25e8996a2a11a65550d15f01baa3ffc768de0d47
Ernest Xu 徐嘉昌 <ernest.xu1024@gmail.com>
2019-07-04 20:36:21 +0100

    MEISVM paper: update feature #12

--- references/MEISVM/f12.m
+++ references/MEISVM/f12.m
@@ -7,9 +7,5 @@
     @return F12: feature 12
 %}
 function F12 = f12(s, K, f, F11)
-    sum_numerator = 0.0;
-    for k=1:K
-        sum_numerator = sum_numerator + (f(k) - F11) .^ 2 * s(k);
-    end
-    F12 = sqrt(sum_numerator / K);
+    F12 = sqrt(sum(((f - F11) .^ 2) .* s) / K);
 end
