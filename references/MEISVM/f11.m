973ad055d963f0ea6111e267a02bbfd68a3f17bd
Ernest Xu 徐嘉昌 <ernest.xu1024@gmail.com>
2019-07-04 20:31:10 +0100

    MEISVM paper: update feature #11

--- references/MEISVM/f11.m
+++ references/MEISVM/f11.m
@@ -6,9 +6,5 @@
     @return F11: feature 11
 %}
 function F11 = f11(s, K, f)
-    sum_numerator = 0.0;
-    for k=1:K
-        sum_numerator = sum_numerator + f(k) * s(k);
-    end
-    F11 = sum_numerator / sum(s);
+    F11 = sum(f .* s) / sum(s);
 end
