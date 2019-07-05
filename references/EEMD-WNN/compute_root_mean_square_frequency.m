dec2db5ab60108d4277adc64ec81dd6509df098b
Ernest Xu 徐嘉昌 <ernest.xu1024@gmail.com>
2019-07-05 08:30:07 +0100

    EEMD-WNN paper: update the method to compute root mean square frequency

--- references/EEMD-WNN/compute_root_mean_square_frequency.m
+++ references/EEMD-WNN/compute_root_mean_square_frequency.m
@@ -1,6 +1,6 @@
-function f = compute_root_mean_square_frequency(x, sf, z)
+function f_rms = compute_root_mean_square_frequency(x, sf, z)
     N = size(x, 1);
     [f, ~] = compute_spectrum(x, N, sf, z);
-    f = sqrt(sum(f .^ 2) / N);
+    f_rms = sqrt(sum(f .^ 2) / N);
 end
 
