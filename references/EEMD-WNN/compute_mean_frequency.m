9139e9c5aa5802d0cba75f5098eab2a5b87f96ef
Ernest Xu 徐嘉昌 <ernest.xu1024@gmail.com>
2019-07-05 08:24:52 +0100

    EEMD-WNN paper: update the method to compute mean frequency

--- references/EEMD-WNN/compute_mean_frequency.m
+++ references/EEMD-WNN/compute_mean_frequency.m
@@ -1,6 +1,6 @@
-function f = compute_mean_frequency(x, sf)
+function f = compute_mean_frequency(x, sf, z)
     N = size(x, 1);
-    [f, ~] = compute_spectrum(x, N, sf, 1);
+    [f, ~] = compute_spectrum(x, N, sf, z);
     f = mean(f);
 end
 
