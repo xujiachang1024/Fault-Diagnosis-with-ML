bae78f82523e41e934477dbed8424e87af8d7bc9
Ernest Xu 徐嘉昌 <ernest.xu1024@gmail.com>
2019-07-05 20:16:38 +0100

    Extract frequency-domain feature #14 from the CWRU data set

---
+++ src/features/CWRU/a_stats_F14_freq_extraction.m
@@ -0,0 +1,17 @@
+clear;
+
+load('data/interim/CWRU/a_combined.mat');
+[N, M] = size(X);
+sf = 12000;
+
+% extract frequency-domain feature #14
+for z=[0 0.5 1 1.5 2]
+    F = zeros(1, M);
+    for m=1:M
+        [f, s] = compute_spectrum(X(:, m), N, sf, z);
+        [K, ~] = size(s);
+        F(:, m) = f14(s, K, f);
+    end
+    data_sink_path = strcat('data/processed/CWRU/a_stats_F14_freq_z', num2str(z), '.mat');
+    save(data_sink_path, 'F', 'Y');
+end 
