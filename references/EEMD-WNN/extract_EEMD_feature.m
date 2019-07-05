10fa357f867b467280d83fbd2230ab29db18d953
Ernest Xu 徐嘉昌 <ernest.xu1024@gmail.com>
2019-07-05 08:38:14 +0100

    EEMD-WNN paper: update the method to extract EEMD feature

--- references/EEMD-WNN/extract_EEMD_feature.m
+++ references/EEMD-WNN/extract_EEMD_feature.m
@@ -1,13 +1,15 @@
 %{
     Select sensitive IMF after EEMD algoirithm
-    @param A:the sensitive IMF (N, 1, M)
+    @param A: the sensitive IMF (N, 1, M)
+    @param sf: the sampling frequency
+    @param z: the z-score
     @return 
 %}
-function F = extract_EEMD_feature(A)
+function F = extract_EEMD_feature(A, sf, z)
     % N: the number of data points in each sample
     % M: the number of samples
     [N, ~, M] = size(A);
-    F = zeros(10, M);
+    F = zeros(7, M);
     for m=1:M
         % feature 1: standard deviation
         F(1, m) = std(A(:, 1, m));
@@ -18,9 +20,11 @@ function F = extract_EEMD_feature(A)
         % feature 4: impulse factor
         F(4, m) = max(abs(A(:, 1, m))) / (sum(abs(A(:, 1, m))) / N);
         % feature 5: mean frequency
+        F(5, m) = compute_mean_frequency(x, sf, z);
         % feature 6: root mean square frequency
+        F(6, m) = compute_root_mean_square_frequency(x, sf, z);
         % feature 7: standard deviation frequency
-        % feature 8: spectrum peak ratio
+        F(7, m) = compute_standard_deviation_frequency(x, sf, z);
     end 
 end
 
