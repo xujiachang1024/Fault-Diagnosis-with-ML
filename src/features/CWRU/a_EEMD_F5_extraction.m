fed872bc2a47eacd9b8cb42acfd7907719fd16f6
Ernest Xu 徐嘉昌 <ernest.xu1024@gmail.com>
2019-07-06 09:40:35 +0100

    Extract EEMD feature #5 from the CWRU data set

---
+++ src/features/CWRU/a_EEMD_F5_extraction.m
@@ -0,0 +1,20 @@
+clear;
+
+load('data/interim/CWRU/a_combined.mat');
+[N, M] = size(X);
+M_ens = 10;
+I_imf = 4;
+sf = 12000;
+
+% extract EEMD feature #5
+for z=[0 0.5 1 1.5 2]
+    A = zeros(N, I_imf, M);
+    for m=1:M
+        [A(:, :, m), ~] = generate_ensemble_IMF(X(:, m), M_ens, I_imf);
+    end
+    A_sen = A(:, 1, :);
+    F_EEMD = extract_EEMD_feature(A_sen, sf, z);
+    F = F_EEMD(5, :);
+    data_sink_path = strcat('data/processed/CWRU/a_EEMD_F5_z', num2str(z), '.mat');
+    save(data_sink_path, 'F', 'Y');
+end
