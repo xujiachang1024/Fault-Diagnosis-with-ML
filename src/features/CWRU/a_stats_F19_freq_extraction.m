e9c1272002354f68c1daead9cd5d7a5a4fdefb07
Ernest Xu 徐嘉昌 <ernest.xu1024@gmail.com>
2019-07-06 22:07:25 +0100

    Extract frequency-domain feature #19 from the CWRU data set

--- src/features/CWRU/a_stats_F19_freq_extraction.m
+++ src/features/CWRU/a_stats_F19_freq_extraction.m
@@ -12,7 +12,7 @@ for z=[0 0.5 1 1.5 2]
         [K, ~] = size(s);
         F11 = f11(s, K, f);
         F12 = f12(s, K, f, F11);
-        F(:, m) = f19(s, K, f, F11, F12);
+        F(:, m) = real(f19(s, K, f, F11, F12));
     end
     data_sink_path = strcat('data/processed/CWRU/a_stats_F19_freq_z', num2str(z), '.mat');
     save(data_sink_path, 'F', 'Y');
