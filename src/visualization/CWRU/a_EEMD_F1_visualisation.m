d0cb602406f5ac17f1e990c0471ab4fdad07d392
Ernest Xu 徐嘉昌 <ernest.xu1024@gmail.com>
2019-07-07 08:29:53 +0100

    Visualise extracted features from the CWRU data set

--- src/visualization/CWRU/a_EEMD_F1_visualisation.m
+++ src/visualization/CWRU/a_EEMD_F1_visualisation.m
@@ -15,6 +15,7 @@ load('data/processed/CWRU/a_EEMD_F1_z2.mat');
 F_stacked(5, :) = F;
 
 F_avg = mean(F_stacked, 1);
+F_med = median(F_stacked, 1);
 
 for z=1:Z
     fig = figure('Position', [0 0 400 300]);hold on
@@ -40,6 +41,19 @@ plot(F_avg(:, 401:500), 'Marker' , '^');
 plot(F_avg(:, 501:600), 'Marker' , 'p');
 plot(F_avg(:, 601:700), 'Marker' , 'h');
 title('CWRU Data: EEMD Feature #1, average');
+xlabel('Sample point');
+ylabel('F1');
+legend('Normal', 'OR 014', 'IR 007', 'IR 014', 'IR 021', 'IR 028', 'B 014', 'Location', 'northeastoutside');
+
+fig = figure('Position', [0 0 400 300]);hold on
+plot(F_med(:, 001:100), 'Marker' , 'o');
+plot(F_med(:, 101:200), 'Marker' , '+');
+plot(F_med(:, 201:300), 'Marker' , '*');
+plot(F_med(:, 301:400), 'Marker' , 's');
+plot(F_med(:, 401:500), 'Marker' , '^');
+plot(F_med(:, 501:600), 'Marker' , 'p');
+plot(F_med(:, 601:700), 'Marker' , 'h');
+title('CWRU Data: EEMD Feature #1, median');
 xlabel('Sample point');
 ylabel('F1');
 legend('Normal', 'OR 014', 'IR 007', 'IR 014', 'IR 021', 'IR 028', 'B 014', 'Location', 'northeastoutside');
