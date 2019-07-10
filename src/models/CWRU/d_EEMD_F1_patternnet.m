dd02a194717e4e2fc215751f205821dadd03ff9e
Ernest Xu 徐嘉昌 <ernest.xu1024@gmail.com>
2019-07-10 10:28:53 +0100

    Implement the 3-layer patternnets based on different features on the CWRU data set, load=3HP

---
+++ src/models/CWRU/d_EEMD_F1_patternnet.m
@@ -0,0 +1,27 @@
+clear;
+Z = 5;
+M = 700;
+F_stacked = zeros(Z + 1, M);
+
+% load the CWRU data set, load=3HP, and stack features
+load('data/processed/CWRU/d_EEMD_F1_z0.mat');
+F_stacked(1, :) = F;
+load('data/processed/CWRU/d_EEMD_F1_z0.5.mat');
+F_stacked(2, :) = F;
+load('data/processed/CWRU/d_EEMD_F1_z1.mat');
+F_stacked(3, :) = F;
+load('data/processed/CWRU/d_EEMD_F1_z1.5.mat');
+F_stacked(4, :) = F;
+load('data/processed/CWRU/d_EEMD_F1_z2.mat');
+F_stacked(5, :) = F;
+F_stacked(6, :) = mean(F_stacked, 1);
+
+% set random seed
+setdemorandstream(491218382)
+
+% initialise the pattern neural network
+net = patternnet([10], 'traingdx', 'crossentropy');
+
+% train the pattern neural network
+[net,tr] = train(net, F_stacked, T_onehot);
+nntraintool
---
+++ src/models/CWRU/d_top3_average_patternnet.m
@@ -0,0 +1,54 @@
+clear;
+Z = 5;
+M = 700;
+F_stacked = zeros(Z, M);
+F_avg = zeros(3, M);
+
+% load the CWRU data set, load=3HP, and average EEMD feature #5
+load('data/processed/CWRU/d_EEMD_F5_z0.mat');
+F_stacked(1, :) = F;
+load('data/processed/CWRU/d_EEMD_F5_z0.5.mat');
+F_stacked(2, :) = F;
+load('data/processed/CWRU/d_EEMD_F5_z1.mat');
+F_stacked(3, :) = F;
+load('data/processed/CWRU/d_EEMD_F5_z1.5.mat');
+F_stacked(4, :) = F;
+load('data/processed/CWRU/d_EEMD_F5_z2.mat');
+F_stacked(5, :) = F;
+F_avg(1, :) = mean(F_stacked, 1);
+
+% load the CWRU data set, load=3HP, and average EEMD feature #6
+load('data/processed/CWRU/d_EEMD_F6_z0.mat');
+F_stacked(1, :) = F;
+load('data/processed/CWRU/d_EEMD_F6_z0.5.mat');
+F_stacked(2, :) = F;
+load('data/processed/CWRU/d_EEMD_F6_z1.mat');
+F_stacked(3, :) = F;
+load('data/processed/CWRU/d_EEMD_F6_z1.5.mat');
+F_stacked(4, :) = F;
+load('data/processed/CWRU/d_EEMD_F6_z2.mat');
+F_stacked(5, :) = F;
+F_avg(2, :) = mean(F_stacked, 1);
+
+% load the CWRU data set, load=3HP, and average frequency-domain feature #14
+load('data/processed/CWRU/d_stats_F14_freq_z0.mat');
+F_stacked(1, :) = F;
+load('data/processed/CWRU/d_stats_F14_freq_z0.5.mat');
+F_stacked(2, :) = F;
+load('data/processed/CWRU/d_stats_F14_freq_z1.mat');
+F_stacked(3, :) = F;
+load('data/processed/CWRU/d_stats_F14_freq_z1.5.mat');
+F_stacked(4, :) = F;
+load('data/processed/CWRU/d_stats_F14_freq_z2.mat');
+F_stacked(5, :) = F;
+F_avg(3, :) = mean(F_stacked, 1);
+
+% set random seed
+setdemorandstream(491218382)
+
+% initialise the pattern neural network
+net = patternnet([10], 'traingdx', 'crossentropy');
+
+% train the pattern neural network
+[net,tr] = train(net, F_avg, T_onehot);
+nntraintool
