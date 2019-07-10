faf6ee6eb84c76f0219b23fe585b5323312e8970
Ernest Xu 徐嘉昌 <ernest.xu1024@gmail.com>
2019-07-10 11:13:02 +0100

    Implement the 3-layer patternnets based on different features on the CWRU data set, variable loads

--- src/models/CWRU/vload_EEMD_F1_patternnet.m
+++ src/models/CWRU/vload_EEMD_F1_patternnet.m
@@ -1,10 +1,10 @@
 clear;
 Z = 5;
 M = 700;
-F_stacked = zeros(Z + 1, M * 2);
-T_stacked = zeros(7, M * 2);
+F_stacked = zeros(Z + 1, M * 3);
+T_stacked = zeros(7, M * 3);
 
-% load the CWRU data set, load=1HP & load=2HP, and stack features
+% load the CWRU data set, load=1HP & load=2HP & load=3HP, and stack features
 load('data/processed/CWRU/a_EEMD_F1_z0.mat');
 F_stacked(1, 1:M) = F;
 load('data/processed/CWRU/a_EEMD_F1_z0.5.mat');
@@ -25,10 +25,21 @@ load('data/processed/CWRU/c_EEMD_F1_z1.5.mat');
 F_stacked(4, 701:M*2) = F;
 load('data/processed/CWRU/c_EEMD_F1_z2.mat');
 F_stacked(5, 701:M*2) = F;
+load('data/processed/CWRU/d_EEMD_F1_z0.mat');
+F_stacked(1, 1401:M*3) = F;
+load('data/processed/CWRU/d_EEMD_F1_z0.5.mat');
+F_stacked(2, 1401:M*3) = F;
+load('data/processed/CWRU/d_EEMD_F1_z1.mat');
+F_stacked(3, 1401:M*3) = F;
+load('data/processed/CWRU/d_EEMD_F1_z1.5.mat');
+F_stacked(4, 1401:M*3) = F;
+load('data/processed/CWRU/d_EEMD_F1_z2.mat');
+F_stacked(5, 1401:M*3) = F;
 F_stacked(6, :) = mean(F_stacked, 1);
 
 T_stacked(:, 1:M) = T_onehot;
 T_stacked(:, 701:M*2) = T_onehot;
+T_stacked(:, 1401:M*3) = T_onehot;
 
 % set random seed
 setdemorandstream(491218382)
--- src/models/CWRU/vload_top3_average_patternnet.m
+++ src/models/CWRU/vload_top3_average_patternnet.m
@@ -1,10 +1,10 @@
 clear;
 Z = 5;
 M = 700;
-F_stacked = zeros((Z + 1) * 3, M * 2);
-T_stacked = zeros(7, M * 2);
+F_stacked = zeros((Z + 1) * 3, M * 3);
+T_stacked = zeros(7, M * 3);
 
-% load the CWRU data set, load=1HP & load=2HP, and stack EEMD feature #5
+% load the CWRU data set, load=1HP & load=2HP & load=3HP, and stack EEMD feature #5
 load('data/processed/CWRU/a_EEMD_F5_z0.mat');
 F_stacked(1, 1:M) = F;
 load('data/processed/CWRU/a_EEMD_F5_z0.5.mat');
@@ -25,9 +25,19 @@ load('data/processed/CWRU/c_EEMD_F5_z1.5.mat');
 F_stacked(4, 701:M*2) = F;
 load('data/processed/CWRU/c_EEMD_F5_z2.mat');
 F_stacked(5, 701:M*2) = F;
+load('data/processed/CWRU/d_EEMD_F5_z0.mat');
+F_stacked(1, 1401:M*3) = F;
+load('data/processed/CWRU/d_EEMD_F5_z0.5.mat');
+F_stacked(2, 1401:M*3) = F;
+load('data/processed/CWRU/d_EEMD_F5_z1.mat');
+F_stacked(3, 1401:M*3) = F;
+load('data/processed/CWRU/d_EEMD_F5_z1.5.mat');
+F_stacked(4, 1401:M*3) = F;
+load('data/processed/CWRU/d_EEMD_F5_z2.mat');
+F_stacked(5, 1401:M*3) = F;
 F_stacked(6, :) = mean(F_stacked(1:5, :), 1);
 
-% load the CWRU data set, load=1HP & load=2HP, and stack EEMD feature #6
+% load the CWRU data set, load=1HP & load=2HP & load=3HP, and stack EEMD feature #6
 load('data/processed/CWRU/a_EEMD_F6_z0.mat');
 F_stacked(7, 1:M) = F;
 load('data/processed/CWRU/a_EEMD_F6_z0.5.mat');
@@ -48,9 +58,19 @@ load('data/processed/CWRU/c_EEMD_F6_z1.5.mat');
 F_stacked(10, 701:M*2) = F;
 load('data/processed/CWRU/c_EEMD_F6_z2.mat');
 F_stacked(11, 701:M*2) = F;
+load('data/processed/CWRU/d_EEMD_F6_z0.mat');
+F_stacked(7, 1401:M*3) = F;
+load('data/processed/CWRU/d_EEMD_F6_z0.5.mat');
+F_stacked(8, 1401:M*3) = F;
+load('data/processed/CWRU/d_EEMD_F6_z1.mat');
+F_stacked(9, 1401:M*3) = F;
+load('data/processed/CWRU/d_EEMD_F6_z1.5.mat');
+F_stacked(10, 1401:M*3) = F;
+load('data/processed/CWRU/d_EEMD_F6_z2.mat');
+F_stacked(11, 1401:M*3) = F;
 F_stacked(12, :) = mean(F_stacked(7:11, :), 1);
 
-% load the CWRU data set, load=1HP & load=2HP, and stack frequency-domain feature #14
+% load the CWRU data set, load=1HP & load=2HP & load=3HP, and stack frequency-domain feature #14
 load('data/processed/CWRU/a_stats_F14_freq_z0.mat');
 F_stacked(13, 1:M) = F;
 load('data/processed/CWRU/a_stats_F14_freq_z0.5.mat');
@@ -71,10 +91,21 @@ load('data/processed/CWRU/c_stats_F14_freq_z1.5.mat');
 F_stacked(16, 701:M*2) = F;
 load('data/processed/CWRU/c_stats_F14_freq_z2.mat');
 F_stacked(17, 701:M*2) = F;
+load('data/processed/CWRU/d_stats_F14_freq_z0.mat');
+F_stacked(13, 1401:M*3) = F;
+load('data/processed/CWRU/d_stats_F14_freq_z0.5.mat');
+F_stacked(14, 1401:M*3) = F;
+load('data/processed/CWRU/d_stats_F14_freq_z1.mat');
+F_stacked(15, 1401:M*3) = F;
+load('data/processed/CWRU/d_stats_F14_freq_z1.5.mat');
+F_stacked(16, 1401:M*3) = F;
+load('data/processed/CWRU/d_stats_F14_freq_z2.mat');
+F_stacked(17, 1401:M*3) = F;
 F_stacked(18, :) = mean(F_stacked(13:17, :), 1);
 
 T_stacked(:, 1:M) = T_onehot;
 T_stacked(:, 701:M*2) = T_onehot;
+T_stacked(:, 1401:M*3) = T_onehot;
 
 F_avg = F_stacked([6 12 18], :);
 
